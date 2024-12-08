fcb706c6b827ef80255d4c3ea92a71071edbb9f8
Felipe Gruendemann <gruendemannf@gmail.com>
2024-12-08 21:16:46 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <iframe src="https://open.spotify.com/embed/track/1uhFZesEqwkIfhlyY9PFRP?utm_source=generator&theme=0" width="100%" height="152" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+}
+
+export default Home;
