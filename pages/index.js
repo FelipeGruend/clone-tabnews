d8397a81bf65ddda447757a006d279f22042504e
Felipe Gruendemann <gruendemannf@gmail.com>
2024-12-16 00:02:17 +0000

    Adicionando a mensagem do desafio no código

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
-function Home() {
-    return <iframe src="https://open.spotify.com/embed/track/1uhFZesEqwkIfhlyY9PFRP?utm_source=generator&theme=0" width="100%" height="152" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="home-container">
+      <h1>Te amo, Babe ❤️!</h1>
+      <p>Aperta o play ⬇️</p>
+      <div>
+      <iframe
+        src="https://open.spotify.com/embed/track/1uhFZesEqwkIfhlyY9PFRP?utm_source=generator&theme=0"
+        width="100%"
+        height="352"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        autoplay="1"></iframe>
+      </div>
+    </div>
+  );
 }
 
-export default Home;
+export default HomePage;
