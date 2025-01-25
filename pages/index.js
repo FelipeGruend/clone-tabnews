dd9fedc9aca0aa4f95cbedd28ff4fa9c91e709a2
Felipe Gruendemann <gruendemannf@gmail.com>
2025-01-25 11:53:37 +0000

    Adiciona estrutura à homepage

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,24 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <div className="home-container">
-      <h1>Te amo, Babe ❤️!</h1>
-      <p>Aperta o play ⬇️</p>
-      <div>
-      <iframe
-        src="https://open.spotify.com/embed/track/1uhFZesEqwkIfhlyY9PFRP?utm_source=generator&theme=0"
-        width="100%"
-        height="352"
-        allowfullscreen=""
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-        autoplay="1"></iframe>
-      </div>
-    </div>
+    <html lang='pt-br'>
+      <body>
+        <div className="home-container">
+          <h1>Te amo, Babe ❤️!</h1>
+          <p>Aperta o play ⬇️</p>
+          <div>
+          <iframe
+            src="https://open.spotify.com/embed/track/1uhFZesEqwkIfhlyY9PFRP?utm_source=generator&theme=0"
+            width="100%"
+            height="352"
+            allowfullscreen=""
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+            autoplay="1"></iframe>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
 
